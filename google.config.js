01711670f090c5e39b5e3d1a49493b219e3bf6b6
janithwanni <janithcwanni@gmail.com>
2024-09-10 16:54:31 +1000

    add data files

--- google.config.js
+++ google.config.js
@@ -1,11 +1,12 @@
 export default [
 	{
-		"id": "1352iFuTSDDFPNAXBbOaEXkF8mCdjPu6B43hpUF2P3C4",
-		"filepath": "src/data/copy.json"
+		id: "1e-ODV4cOur0-AJnfCXTbVrG6nd_4BTR0NPh_XdX4fqc",
+		filepath: "src/data/copy.json"
 	},
 	{
-		"id": "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
-		"gid": "0",
-		"filepath": "src/data/test.csv"
+		id: "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
+		gid: "0",
+		filepath: "src/data/test.csv"
 	}
-]
+];
+
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,11 +1,11 @@
 import cross from "$data/cross.csv";
-import time from "$data/time.csv"
+import time from "$data/time.csv";
 
 export async function load() {
-  let crossData = cross.map(x => ({
-    ...x,
-    class: x.class == "9" ? "A" : "B"
-  }));
-  let data = { cross: crossData, time: time }
-  return { data };
+	let crossData = cross.map((x) => ({
+		...x,
+		class: x.class == "9" ? "A" : "B"
+	}));
+	let data = { cross: crossData, time: time };
+	return { data };
 }
